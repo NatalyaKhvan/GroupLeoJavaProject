54e419d5c7668fe20d36c0bdebf35d5bdcfe5532
farabshaikh <farabshaikh@cydeo.com>
2023-05-25 19:35:05 -0500

    Created OddOrEven project

---
+++ src/fara/DivideWithoutOperator.java
@@ -0,0 +1,27 @@
+package fara;
+
+
+
+
+public class DivideWithoutOperator {
+    public static void Divide2Numbers() {
+
+        int dividend = 50;
+        int divisor = 9;
+
+        int quotient = 0;
+
+        while (dividend >= divisor) {
+            dividend -= divisor;
+            quotient++;
+        }
+
+        System.out.println(quotient + " with a remainder of " + dividend);
+
+    }
+
+    public static void main(String[] args) {
+        Divide2Numbers();
+
+    }
+}
