bf36700f08ae64ba539f00f0f71e6fd7b1a5dc4a
tjk <yfarangis@gmail.com>
2023-05-27 15:04:02 +0500

    Added a two more tasks

---
+++ src/farangez/FINRA.java
@@ -0,0 +1,18 @@
+package farangez;
+
+public class FINRA {
+
+    public static void FINRA() {
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else {
+                System.out.print("RA ");
+
+            }
+        }
+    }
+}
